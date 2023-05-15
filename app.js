222b2255a97c677284f4e4c793aff836409a8e08
Kashaf Ansari <kashafansari3108@gmail.com>
2023-05-15 20:04:26 +0500

    todo-app backened

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const router = require('./routes/routes');
 const cors = require('cors');
+const connectDb = require('./db/db');
+require('dotenv').config({path:'./config.env'});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const BASE_URI = "mongodb+srv://admin:admin3108@cluster0.zbhlrew.mongodb.net/TodoApp";
 
-mongoose
-  .connect(BASE_URI)
-  .then((res) => console.log("Database Connected"))
-  .catch((err) => console.log(err));
-
+connectDb();
 app.use(express.json());
 app.use(cors());
 app.use("/api",router);
